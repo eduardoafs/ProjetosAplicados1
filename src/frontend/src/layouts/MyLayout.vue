4627ebf1c52aa36324282620ffa4c1f0d32c1a3e
Arthur Ribeiro <devarthurribeiro@gmail.com>
2019-08-24 01:07:14 -0300

    Criação do projeto de frontend
    O projeto foi iniciado com [Quasar](http://quasar.dev) para auxiliar na criação das
    interfaces, possibilitando um desenvolvimento facilitado.
    
    Foi adicionado o ESlint para garantir o padrão do código, o Vuex para gerência o estado da aplicação, Axios para fazer as requisições http.

---
+++ src/frontend/src/layouts/MyLayout.vue
@@ -0,0 +1,55 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn flat dense round @click="leftDrawerOpen = !leftDrawerOpen" aria-label="Menu">
+          <q-icon name="menu" />
+        </q-btn>
+
+        <q-toolbar-title>
+          GES
+        </q-toolbar-title>
+
+        <div></div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" bordered content-class="bg-grey-2">
+      <q-list>
+        <q-item-label header>Menu</q-item-label>
+        <q-item clickable tag="a" target="_blank" href="https://github.com/eduardoafs/ProjetosAplicados1">
+          <q-item-section avatar>
+            <q-icon name="code" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Github</q-item-label>
+            <q-item-label caption>github.com/quasarframework</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop
+    }
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style>
+</style>
