cf1993218c0e4ded586a80095948f0554e68adfe
Arthur Ribeiro <devarthurribeiro@gmail.com>
2019-09-05 14:15:21 -0600

    ajuste na tela inicial

--- src/frontend/src/pages/Index.vue
+++ src/frontend/src/pages/Index.vue
@@ -1,10 +1,27 @@
 <template>
   <q-page class="flex flex-center">
-    <h1>Ges</h1>
+    <div class="home text-center">
+      <h3>Reserva de espaços</h3>
+
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Amet vero voluptate asperiores fuga dicta rerum, consequuntur
+        incidunt consequatur odio odit nihil dolorem temporibus corrupti qui quae
+        quibusdam maxime reprehenderit ullam.
+      </p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Amet vero voluptate asperiores fuga dicta rerum, consequuntur
+        incidunt consequatur odio odit nihil dolorem temporibus corrupti qui quae
+        quibusdam maxime reprehenderit ullam.
+      </p>
+    </div>
+
   </q-page>
 </template>
 
 <style>
+.home {
+  width: 60%;
+}
 </style>
 
 <script>
