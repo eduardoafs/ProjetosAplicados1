823321c6bd1f05f1caa69150ab05f2b5ea109a03
Heverton13 <heverton.gomes2018@gmail.com>
2019-09-16 16:35:19 -0300

    Inicio de produção de tela reserva recorrente

---
+++ src/frontend/src/pages/CurrentRecurrence.vue
@@ -0,0 +1,70 @@
+<template>
+  <q-page padding>
+    <div class="text-h5">Reseva Recorrente</div>
+    <br>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="row q-gutter-md">
+          <div class="col">
+            <q-card>
+              <q-card-section class="bg-primary text-white">
+                <div class="text-h6">Selecionar Data</div>
+              </q-card-section>
+              <div class="q-pa-md q-gutter-md row items-start">
+                <div class="col">
+                  <q-input
+                    v-model="dateInit"
+                    mask="##/##/####"
+                    stack-label
+                    label="Data - Início"
+                  />
+                </div>
+                <div class="col">
+                  <q-input
+                    v-model="dataEnd"
+                    mask="##/##/####"
+                    stack-label
+                    label="Data - Término"
+                  />
+                </div>
+                <div class="col">
+                  <q-input
+                    v-model="timeInit"
+                    mask="##:##"
+                    stack-label
+                    label="Hora - Início"
+                  />
+                </div>
+                <div class="col">
+                  <q-input
+                    v-model="timeEnd"
+                    mask="##:##"
+                    stack-label
+                    label="Hora - Término"
+                  />
+                </div>
+              </div>
+            </q-card>
+          </div>
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'CurrentRecurrence',
+  data: () => ({
+    reserve: {
+      dateInit: '',
+      dateEnd: '',
+      timeInit: '',
+      timeEnd: ''
+    }
+  })
+}
+</script>
+
+<style scoped>
+</style>
